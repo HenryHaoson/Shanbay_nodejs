0c2d8ed7ccd155ae2fe1abd220b912870c34d0ab
Henryhaoson <740227705@qq.com>
2017-07-25 16:02:51 +0800

    groupDAL

--- dal/groupDAL.js
+++ dal/groupDAL.js
@@ -1,16 +1,16 @@
 var db = require('../db/db');
 
 exports.addGroup = function (groupInfo, callback) {
-    let sql = "INSERT INTO user set ";
+    let sql = "INSERT INTO group set ";
 
     let addSql = '';
 
-    for (let key in userInfo) {
-        if (userInfo[key] !== '') {
-            if(addSql.length === 0) {
-                addSql += key + " = '" + userInfo[key] + "'";
+    for (let key in groupInfo) {
+        if (groupInfo[key] !== '') {
+            if (addSql.length === 0) {
+                addSql += key + " = '" + groupInfo[key] + "'";
             } else {
-                addSql += ' , ' + key + " = '" + userInfo[key] + "'";
+                addSql += ' , ' + key + " = '" + groupInfo[key] + "'";
             }
         }
     }
@@ -19,20 +19,77 @@ exports.addGroup = function (groupInfo, callback) {
 
     console.log('增加用户信息：' + sql);
 
-    db.mysqlPool.getConnection(function(err, connection) {
+    db.mysqlPool.getConnection(function (err, connection) {
         if (err) {
-            errAlert('数据库连接失败！' + err, 'userDAL.addUserInfo()');
+            errAlert('数据库连接失败！' + err, 'groupDAL.addGroup()');
             return callback(true, '连接数据库失败');
         }
 
-        connection.query(sql, function(err, results) {
+        connection.query(sql, function (err, results) {
             connection.release();
 
             if (err) {
-                errAlert('sql语句：' + err, 'userDAL.addUserInfo()');
+                errAlert('sql语句：' + err, 'groupDAL.addGroup()');
                 return callback(true, '增加用户失败');
             }
             return callback(false, results);
         });
     });
 };
+
+exports.queryGroup = function (data, callback) {
+    var sql = 'select groupId , groupName ,groupDescription ,groupBirth ,leaderId ,leaderName ,groupUrl ' +
+        'from group where 1=1';
+
+    for (let key in data) {
+        if (data[key] !== '') {
+            sql += ' and ' + key + " = '" + data[key] + "'";
+        }
+    }
+
+    console.log('查询小组信息：' + sql);
+
+    db.mysqlPool.getConnection(function (err, connection) {
+        if (err) {
+            errAlert('数据库连接失败！' + err, 'groupDAL.queryGroup()');
+            return callback(true, '连接数据库失败');
+        }
+
+        connection.query(sql, function (err, results) {
+            connection.release();
+
+            if (err) {
+                errAlert('sql语句：' + err, 'groupDAL.queryGroup()');
+                return callback(true, '查询失败');
+            }
+            return callback(false, results);
+        });
+    });
+};
+
+exports.deleteGroup = function (data, callback) {
+    var sql = 'delete from group where 1=1';
+    for (let key in data) {
+        if (data[key] !== '') {
+            sql += ' and ' + key + " = '" + data[key] + "'";
+        }
+    }
+    console.log('删除小组：'+sql);
+
+    db.mysqlPool.getConnection(function (err,connection){
+        if(err){
+            errAlert('数据库连接失败'+err,'groupDAL.deleteGroup()');
+            return callback(true,'数据库连接失败');
+        }
+
+        connection.query(sql,function (err,results) {
+            connection.release();
+
+            if(err){
+                errAlert('sql语句：'+err,'groupDAL.deleteGroup');
+                return callback(true,"删除失败");
+            }
+            return callback(false,results);
+        });
+    });
+}
